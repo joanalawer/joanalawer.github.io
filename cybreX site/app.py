33fb4df206372fbb57a3844f71b7a6a60e749cfd
Joana <lawerjoana477@gmail.com>
2024-08-29 13:59:41 +0000

    Changed home function to index

--- cybreX site/app.py
+++ cybreX site/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request
 import pandas as pd 
-# import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 # from sklearn.externals import joblib
@@ -9,7 +8,7 @@ from sklearn.naive_bayes import MultinomialNB
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
 	return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
